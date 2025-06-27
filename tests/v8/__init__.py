9602390bb5ba7e62e17909119c6f7240a654ad9a
root <root@Ubuntu-16.04-amd64.qcow2>
2025-06-27 09:20:16 +0200

    Initial commit

---
+++ tests/v8/__init__.py
@@ -0,0 +1,66 @@
+"""Global variables associated to the test suite."""
+
+#: List of CPP variables that should be defined in config.h in order to enable this suite.
+need_cpp_vars = [
+]
+
+#: List of keywords that are automatically added to all the tests of this suite.
+keywords = [
+]
+
+#: List of input files
+inp_files = [
+"t01.in",
+"t02.in",
+"t03.in",
+"t04.in",
+"t05.in",
+"t06.in",
+"t07.in",
+"t08.in",
+"t09.in",
+"t10.in",
+"t11.in",
+"t12.in",
+"t13.in",
+"t14.in",
+"t30.in",
+"t31.in",
+"t32.in",
+"t33.in",
+"t41.in",
+"t42.in",
+"t43.in",
+"t44.in",
+"t45.in",
+"t46.in",
+"t47.in",
+"t48.in",
+"t49.in",
+"t50.in",
+"t61.in",
+"t62.in",
+"t63.in",
+"t64.in",
+"t65.in",
+"t66.in",
+"t67.in",
+"t68.in",
+"t69.in",
+"t70.in",
+"t71.in",
+"t72.in",
+"t73.in",
+"t74.in",
+"t75.in",
+"t76.in",
+"t77.in",
+"t78.in",
+"t79.in",
+"t80.in",
+#GW-soc
+"t90.in",
+"t91.in",
+"t92.in",
+"t93.in",
+]
